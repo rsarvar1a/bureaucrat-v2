52f2672e0a95a25416d5dd9703999b95eb34b0a7
Rohan Sarvaria <rsarvaria@uwaterloo.ca>
2026-02-12 20:12:49 -0500

    fix(schema): implement feedback; extend snowflakes to control nullability

--- bureaucrat/schema/helpers/snowflakes.ts
+++ bureaucrat/schema/helpers/snowflakes.ts
@@ -1,17 +1,30 @@
-import { integer } from 'drizzle-orm/pg-core';
+import { bigint as pgBigint } from 'drizzle-orm/pg-core';
 
-const intnotnull = () => integer().notNull();
+const bigint = () => pgBigint({ mode: 'number' });
+const bigintnotnull = () => bigint().notNull();
 
 /**
  *  The snowflake object classes relevant to Bureaucrat.
  */
 export type SnowflakeReferenceable = 'guild' | 'category' | 'channel' | 'thread' | 'message' | 'member' | 'role';
 
-type _IntNotNull = ReturnType<typeof intnotnull>;
-type _ColumnSet<T extends SnowflakeReferenceable> = Record<T, _IntNotNull>;
+type _ColumnSet<T extends SnowflakeReferenceable, N> = Record<T, ReturnType<N extends true ? typeof bigint : typeof bigintnotnull>>;
 
 /**
  *  Given a set of Discord object types referenceable by snowflake, produces a subset of non-null integer columns for them.
  */
-export const snowflakes = <T extends SnowflakeReferenceable>(...kinds: T[]): _ColumnSet<T> =>
-  Object.fromEntries(kinds.map((k: T) => [k, intnotnull()])) as _ColumnSet<T>;
+export const snowflakes = <
+  TA extends [true, ...SnowflakeReferenceable[]] | SnowflakeReferenceable[],
+  T extends SnowflakeReferenceable = Exclude<TA[number], true>,
+  N = TA[0] extends true ? true : false,
+>(
+  ...args: TA
+): _ColumnSet<T, N> => {
+  const nullable = (() => {
+    const nullable = args[0] === true;
+    if (nullable) args.shift();
+    return nullable;
+  })();
+
+  return Object.fromEntries((args as T[]).map((k: T) => [k, nullable ? bigint() : bigintnotnull()])) as _ColumnSet<T, N>;
+};
--- bureaucrat/schema/helpers/timestamps.ts
+++ bureaucrat/schema/helpers/timestamps.ts
@@ -3,12 +3,12 @@ import { timestamp } from 'drizzle-orm/pg-core';
 /**
  *  Creates a timestamp column that is automatically defaulted to the current datetime if not provided.
  */
-export const automatictimestamp = () => timestamp().notNull().defaultNow();
+export const automaticTimestamp = () => timestamp().notNull().defaultNow();
 
 /**
  *  Creates a standard set of timestamp columns for record audit purposes.
  */
 export const timestamps = () => ({
-  createdAt: automatictimestamp(),
-  updatedAt: automatictimestamp(),
+  createdAt: automaticTimestamp(),
+  updatedAt: automaticTimestamp().$onUpdate(() => new Date()),
 });

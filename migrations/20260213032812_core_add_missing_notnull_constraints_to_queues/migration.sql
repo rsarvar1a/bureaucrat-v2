77a93fd4d2d7bfa2eded208d057b252110ecdf64
Rohan Sarvaria <rsarvaria@uwaterloo.ca>
2026-02-13 19:54:43 -0500

    chore(schema; core): squash migrations for core.queues (#4)

--- migrations/20260213032812_core_add_missing_notnull_constraints_to_queues/migration.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "core"."QueueEntry" ALTER COLUMN "queue" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "core"."QueueEntry" ALTER COLUMN "minimumStartDate" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "core"."QueueEntry" ALTER COLUMN "public" SET NOT NULL;

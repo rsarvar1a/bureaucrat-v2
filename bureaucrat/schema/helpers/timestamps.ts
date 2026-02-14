3a89d925861656aa281b0ff932008df43acc2642
Rohan Sarvaria <rsarvaria@uwaterloo.ca>
2026-02-13 19:30:53 -0500

    feat(schema; core): model Storyteller queues (#3)
    * feat(schema; core): model Storyteller queues
    
    * fixup! feat(schema; core): model Storyteller queues
    
    * fix(schema; core): core.Queues is a top-level table and requires a guild id

--- bureaucrat/schema/helpers/timestamps.ts
+++ bureaucrat/schema/helpers/timestamps.ts
@@ -3,12 +3,14 @@ import { timestamp } from 'drizzle-orm/pg-core';
 /**
  *  Creates a timestamp column that is automatically defaulted to the current datetime if not provided.
  */
-export const automaticTimestamp = () => timestamp().notNull().defaultNow();
+export const automaticTimestamp = () => timestamp().defaultNow();
 
 /**
  *  Creates a standard set of timestamp columns for record audit purposes.
  */
 export const timestamps = () => ({
-  createdAt: automaticTimestamp(),
-  updatedAt: automaticTimestamp().$onUpdate(() => new Date()),
+  createdAt: automaticTimestamp().notNull(),
+  updatedAt: automaticTimestamp()
+    .notNull()
+    .$onUpdate(() => new Date()),
 });
